70601ee8b5a23af55f5ab889c1908bc53536ab14
Jonas Bergqvist <jonas.bergqvist@episerver.com>
2024-05-26 19:30:05 +0200

    Starting to make _app work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,45 @@
 import "@/styles/globals.css";
-import { createHttpLink, ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { createHttpLink, ApolloClient, InMemoryCache, ApolloProvider, ApolloLink } from "@apollo/client";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  let httpLink = createHttpLink({
-    uri: 'https://beta2.cg.optimizely.com/content/v2?auth=nSabkbOsWUA55R2YBSYvuGCOgfAnEqE5Zah5fTHsaKlm1kQi',
-  });
+  let httpLink: ApolloLink
   let headers: Record<string, string> | undefined = undefined
+  let version: number | null = null
+  let preview_token: string | undefined = undefined
+  if (typeof window !== "undefined" && window.location !== undefined) {
+    const queryString = window?.location?.search;
+    const urlParams = new URLSearchParams(queryString);
+    preview_token = urlParams.get('preview_token') ?? undefined;
+
+    const pathArray = window?.location?.pathname?.split('/')
+    var contentId = pathArray[pathArray.length - 1]
+
+    var contentIdArray = contentId.split('_')
+    if (contentIdArray.length > 1) {
+      version = contentIdArray[contentIdArray.length - 1] as unknown as number
+    }
+  }
+
+  //pageProps.version = version
+
+  if (preview_token) {
+    headers = { Authorization: 'Bearer ' + preview_token }
+    
+    httpLink = createHttpLink({
+      uri: 'https://beta2.cg.optimizely.com/content/v2?cache=false',
+    });
+
+    const communicationScript = document.createElement('script');
+    communicationScript.src = `https://app-ocxcjobe11znb7p003.cms.optimizely.com/Util/javascript/communicationinjector.js`;
+    communicationScript.setAttribute('data-nscript', 'afterInteractive')
+    document.body.appendChild(communicationScript);
+  } else {
+    httpLink = createHttpLink({
+      uri: 'https://beta2.cg.optimizely.com/content/v2?auth=nSabkbOsWUA55R2YBSYvuGCOgfAnEqE5Zah5fTHsaKlm1kQi',
+    });
+  }
 
   let client = new ApolloClient({
     link: httpLink,
