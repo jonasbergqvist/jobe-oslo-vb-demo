245308adfd699a7a9969363a4c5cbfafabc3acff
Jonas Bergqvist <jonas.bergqvist@episerver.com>
2024-05-27 14:24:03 +0200

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -48,9 +48,6 @@ function createApolloClient() {
 }
 */
 export default function App({ Component, pageProps }: AppProps) {
-
-  //const client = createApolloClient()
-
   return (
     <ApolloProvider client={client!}>
       <Component {...pageProps} />
